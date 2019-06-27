e8eb8bdc76bd3dbe68680642a3c7f1cc8bd99759
vmichelan <venanciuslm@gmail.com>
2019-06-26 22:10:36 -0300

    Updates render_contents

--- ui.c
+++ ui.c
@@ -12,23 +12,6 @@
 
 WINDOW *w1,*w2,*w3,*cmdw,*pathw;
 
-int utf8_strlen(char* str,int len)
-{
-    int c,i,ix,q;
-    for (q=0, i=0, ix=len; i < ix; i++, q++)
-    {
-        c = (unsigned char) str[i];
-        if      (c>=0   && c<=127) i+=0;
-        else if ((c & 0xE0) == 0xC0) i+=1;
-        else if ((c & 0xF0) == 0xE0) i+=2;
-        else if ((c & 0xF8) == 0xF0) i+=3;
-        //else if (($c & 0xFC) == 0xF8) i+=4; // 111110bb //byte 5, unnecessary in 4 byte UTF-8
-        //else if (($c & 0xFE) == 0xFC) i+=5; // 1111110b //byte 6, unnecessary in 4 byte UTF-8
-        else return 0;//invalid utf8
-    }
-    return q;
-}
-
 void initui() {
     int yMax,xMax;
     getmaxyx(stdscr,yMax,xMax);
@@ -80,56 +63,52 @@ void render_contents(WINDOW* w,dir* directory) {
     if (!directory) {
         return;
     }
-    int i = 0,j,yMax,xMax,len,count,index = directory->index;
-    char ch;
-    getmaxyx(w,yMax,xMax);
+    int i = 0, yMax, xMax;
+    int cursory, cursorx;
+    getmaxyx(w, yMax, xMax);
     if (directory->size == 0) {
-        mvwprintw(w,i,0," ");
+        wmove(w, 0, 0);
         wclrtoeol(w);
-        wattron(w,COLOR_PAIR(3));
-        wprintw(w,"***EMPTY***");
-        wattroff(w,COLOR_PAIR(3));
+        wattron(w, COLOR_PAIR(3));
+        waddstr(w, "***EMPTY***");
+        wattroff(w, COLOR_PAIR(3));
         i++;
     }
-    while (index < directory->size && i < yMax) {
-        if (directory->cursor == index) {
-            wattron(w,A_STANDOUT);
+    while (i + directory->index < directory->size && i < yMax) {
+        if (directory->cursor == i + directory->index) {
+            wattron(w, A_STANDOUT);
         }
         else {
-            wattroff(w,A_STANDOUT);
+            wattroff(w, A_STANDOUT);
         }   
-        if (directory->type[index] == DT_DIR) {
-            wattron(w,COLOR_PAIR(1));
+        if (directory->type[i + directory->index] == DT_DIR) {
+            wattron(w, COLOR_PAIR(1));
         }
 
-        len = strlen(directory->content[index]);
-        len = utf8_strlen(directory->content[index],len);
+        mvwaddch(w, i, 0, ' ');
+        waddnstr(w, directory->content[i + directory->index], xMax-2);
 
-        mvwprintw(w,i,0," ");
-        if (len > xMax) {
-            ch = directory->content[index][xMax];
-            directory->content[index][xMax] = '\0';
-            mvwprintw(w,i,1,"%s\n",directory->content[index]);
-            directory->content[index][xMax] = ch;
+        getyx(w, cursory, cursorx);
+
+        if (directory->cursor == i + directory->index) {
+            while (cursorx < xMax) {
+                waddch(w, ' ');
+                cursorx++;
+            }
         }
         else {
-            mvwprintw(w,i,1,"%s\n",directory->content[index]);
+            wclrtoeol(w);
         }
 
-        for (j = len+1;j < xMax;j++) {
-            mvwprintw(w,i,j," ");
-        }
-
-        index++;
         i++;
-        wattroff(w,COLOR_PAIR(1));
-   }
+        wattroff(w, COLOR_PAIR(1));
+    }
     while (i < yMax) {
-        wmove(w,i,0);
+        wmove(w, i, 0);
         wclrtoeol(w);
         i++;
     }
-    wattroff(w,A_STANDOUT);
+    wattroff(w, A_STANDOUT);
     wrefresh(w);
 
 }
